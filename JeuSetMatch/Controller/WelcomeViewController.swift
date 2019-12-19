5e02a47bc3dfa71f7006bfed6fbc429da46b129a
Pauline <pauline.nomballais@hotmail.fr>
2019-12-19 14:38:25 +0100

    Modification of WelcomeViewController

--- JeuSetMatch/Controller/WelcomeViewController.swift
+++ JeuSetMatch/Controller/WelcomeViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        titleLabel.text = ""
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        titleLabel.text = "🎾"
         var charIndex = 0.0
-        let titleText = "🎾 Jeu Set & Match 🎾"
+        let titleText = "Jeu Set & Match"
         for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
                 self.titleLabel.text?.append(letter)
+                if self.titleLabel.text == "🎾Jeu Set & Match"{
+                    self.registerButton.isHidden = false
+                    self.logInButton.isHidden = false
+                }
             }
             charIndex += 1
         }
-        /// performsegue
     }
-
 }
