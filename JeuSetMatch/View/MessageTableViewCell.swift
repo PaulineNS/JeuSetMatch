5569acdd7b93ace0b89f48b22f900d7481ae970f
Pauline <pauline.nomballais@hotmail.fr>
2019-12-26 19:22:42 +0100

    Creation of MessageTableViewCell

---
+++ JeuSetMatch/View/MessageTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageTableViewCell.swift
+//  JeuSetMatch
+//
+//  Created by Pauline Nomballais on 26/12/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
