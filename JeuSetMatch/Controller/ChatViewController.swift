ae1ebaac7bf3442808c66f52a27b5b6601bb6ef6
Pauline <pauline.nomballais@hotmail.fr>
2019-12-27 12:34:32 +0100

    Sorted firebase data by date

--- JeuSetMatch/Controller/ChatViewController.swift
+++ JeuSetMatch/Controller/ChatViewController.swift
@@ -28,7 +28,9 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
             self.messages = []
             if let e = error {
                 print("There was an issue retrieving data from Firestore. \(e)")
@@ -53,8 +55,10 @@ class ChatViewController: UIViewController {
             return
         }
         db.collection(K.FStore.collectionName).addDocument(data: [
-            K.FStore.senderField : messageSender,
-            K.FStore.bodyField : messageBody]) { (error) in
+            K.FStore.senderField: messageSender,
+            K.FStore.bodyField: messageBody,
+            K.FStore.dateField: Date().timeIntervalSince1970
+        ]) { (error) in
                 guard let e = error else {
                     print("Successfully saved data.")
                     return
