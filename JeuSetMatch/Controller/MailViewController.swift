a3eb3b7f16650fb4e610eb9e4fa2961b445dc633
Pauline <pauline.nomballais@hotmail.fr>
2019-12-31 11:45:15 +0100

    Creation of MailVC, PasswordVC, BirthdayVC, CityVC and PseudoVC

---
+++ JeuSetMatch/Controller/MailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MailViewController.swift
+//  JeuSetMatch
+//
+//  Created by Pauline Nomballais on 31/12/2019.
+//  Copyright © 2019 PaulineNomballais. All rights reserved.
+//
+
+import UIKit
+
+class MailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
