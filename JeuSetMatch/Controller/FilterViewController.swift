13962567c526eac4eb51bf3b378dd72c2e859d22
Pauline <pauline.nomballais@hotmail.fr>
2020-01-21 10:01:20 +0100

    Managed the picker view in filterTableView

--- JeuSetMatch/Controller/FilterViewController.swift
+++ JeuSetMatch/Controller/FilterViewController.swift
@@ -8,31 +8,18 @@
 
 import UIKit
 
-class FilterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+class FilterViewController: UIViewController {
     
     
     @IBOutlet weak var filterTableView: UITableView!
     
     private let filtersDictionnary = ["Age": "Tout", "Niveau": "Tout", "Sexe": "Tout", "Ville": "Tout"]
     private var filtersArray = [Filters]()
-    private var levelsArray = ["-30 - Pro","-15 - Pro","-4/6 - Pro","-2/6 - Pro","0 - Semi-pro","1/6 - Semi-pro","2/6 - Semi-pro","3/6 - Expert avancé","4/6 - Expert avancé","5/6 - Expert avancé","15 - Expert avancé","15/1 - Expert","15/2 - Expert","15/3 - Expert","15/4 - Compétiteur avancé","15/5 - Compétiteur avancé","30 - Compétiteur","30/1 - Compétiteur","30/2 - Intermédiaire avancé","30/3 - Intermédiaire","30/4 - Intermédiaire","30/5 - Amateur avancé","40 - Amateur","Débutant","Tous les niveaux"]
-    private var gendersArray = ["Les Deux", "Femme", "Homme"]
-    private var genderPicker: UIPickerView?
-    private var levelPicker: UIPickerView?
-    
-    private var levelChoosen = ""
-    private var genderChoosen = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         filterTableView.dataSource = self
         filterTableView.delegate = self
-        levelPicker = UIPickerView()
-        levelPicker?.delegate = self
-        levelPicker?.dataSource = self
-        genderPicker = UIPickerView()
-        genderPicker?.delegate = self
-        genderPicker?.dataSource = self
         filterTableView.register(UINib(nibName: K.filterCellNibName, bundle: nil), forCellReuseIdentifier: K.filterCellIdentifier)
         for (key, value) in filtersDictionnary.sorted(by: { $0.0 < $1.0 }) {
             filtersArray.append(Filters(denomination: key, value: value))
@@ -41,39 +28,6 @@ class FilterViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     
     @IBAction func searchPlayers(_ sender: Any) {
         
-    }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView == levelPicker {
-            return levelsArray.count
-        }
-        if pickerView == genderPicker {
-            return gendersArray.count
-        }
-        return 0
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView == levelPicker {
-            return levelsArray[row]
-        }
-        if pickerView == genderPicker {
-            return gendersArray[row]
-        }
-        return ""
-    }
-    
-    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView == levelPicker {
-            levelChoosen = levelsArray[row]
-            print(levelChoosen)
-        }
-        if pickerView == genderPicker {
-        }
     }
 }
 
@@ -87,13 +41,12 @@ extension FilterViewController: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: K.filterCellIdentifier, for: indexPath) as? FilterTableViewCell else {return UITableViewCell()}
         
         cell.filter = filtersArray[indexPath.row]
-        //cell.delegate = self
         
         if indexPath.row == 1 {
-            cell.filterValueTxtField.inputView = levelPicker
+            cell.filterValueTxtField.inputView = cell.levelPicker
         }
         if indexPath.row == 2 {
-            cell.filterValueTxtField.inputView = genderPicker
+            cell.filterValueTxtField.inputView = cell.genderPicker
         }
         return cell
     }
--- JeuSetMatch/View/FilterTableViewCell.swift
+++ JeuSetMatch/View/FilterTableViewCell.swift
@@ -14,10 +14,21 @@ class FilterTableViewCell: UITableViewCell, UITextFieldDelegate {
     @IBOutlet weak var filterValueTxtField: UITextField!
     @IBOutlet weak var arrow: UILabel!
     
+    private var levelsArray = ["-30 - Pro","-15 - Pro","-4/6 - Pro","-2/6 - Pro","0 - Semi-pro","1/6 - Semi-pro","2/6 - Semi-pro","3/6 - Expert avancé","4/6 - Expert avancé","5/6 - Expert avancé","15 - Expert avancé","15/1 - Expert","15/2 - Expert","15/3 - Expert","15/4 - Compétiteur avancé","15/5 - Compétiteur avancé","30 - Compétiteur","30/1 - Compétiteur","30/2 - Intermédiaire avancé","30/3 - Intermédiaire","30/4 - Intermédiaire","30/5 - Amateur avancé","40 - Amateur","Débutant","Tous les niveaux"]
+    private var gendersArray = ["Les Deux", "Femme", "Homme"]
+    
+    var genderPicker: UIPickerView?
+    var levelPicker: UIPickerView?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         filterValueTxtField.delegate = self
+        levelPicker = UIPickerView()
+        levelPicker?.delegate = self
+        levelPicker?.dataSource = self
+        genderPicker = UIPickerView()
+        genderPicker?.delegate = self
+        genderPicker?.dataSource = self
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -32,7 +43,44 @@ class FilterTableViewCell: UITableViewCell, UITextFieldDelegate {
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-            textField.resignFirstResponder()
-            return true
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
+
+extension FilterTableViewCell: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == levelPicker {
+            return levelsArray.count
         }
+        if pickerView == genderPicker {
+            return gendersArray.count
+        }
+        return 0
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == levelPicker {
+            return levelsArray[row]
+        }
+        if pickerView == genderPicker {
+            return gendersArray[row]
+        }
+        return ""
+    }
+    
+    func pickerView( _ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == levelPicker {
+            filterValueTxtField.text = levelsArray[row]
+        }
+        if pickerView == genderPicker {
+            filterValueTxtField.text = gendersArray[row]
+        }
+    }
 }
